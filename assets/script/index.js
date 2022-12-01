a4e313da43a2aff4d3fe62ad63562db4d72fa2bc
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-30 21:57:27 -0600

    Update index.js update functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,187 +1,86 @@
 'use strict';
-/* 
+
 document.getElementById("buttonstart").addEventListener("click", startgame)
 
-
-function sumarscore() {
-
+function startgame() {
+    document.getElementById("inputypeword").disabled=false;
+    document.getElementById("inputypeword").focus();
+    document.getElementById("inputypeword").value="";
+    seconds = 10;
+    score = 0;
+    document.getElementById("pointscore").innerHTML=score;    
+    document.getElementById("timeremainder").innerHTML = seconds;
+    decrementTimer();
+    randomizewords();
 }
 
+// logical to decrement time
 
-// codigo de funcion decrementar tiempo
+let seconds = 10;
+let timer = '';
 
-let seconds = 99;
-      let timer = setInterval(function () {
+function decrementTimer() {
+    timer = setInterval(function () {
         seconds--;
         if (seconds == 0) {
-          stoptimer();
+            stoptimer();
         }
-        document.getElementById("timeremainder").innerHTML=seconds;
-      }, 1000);
+        document.getElementById("timeremainder").innerHTML = seconds;
+    }, 1000);
+}
 
-      function stoptimer() {
-        clearInterval(timer);
-      }
+function stoptimer() {
+    clearInterval(timer); //stop timer
+    document.getElementById("inputypeword").disabled=true;
+} 
 
+const keys = [ 'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
+'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+'keyboard', 'window' ];
 
+function randomizewords() {
+    let originalLen = keys.length;
 
+    const obj = {};
 
+    for (let i = 0; i < originalLen; i++) {
+      let k = Math.floor(Math.random() * keys.length);
+      //console.log(k + " Palabras Aletorias: " + keys[k]);
+      document.getElementById("showWords").innerHTML = keys[k];
+    }
+    console.log(obj);
+}
 
+// listen gamer type and compare Words
 
-function startgame() {
-    alert('hola');
-    if ("buttonstart" = true) {
+document.getElementById("inputypeword").addEventListener("keyup",comparewords);
 
+let score = 0;
+
+function comparewords(){
+    let currentword = document.getElementById("showWords").textContent.toUpperCase();
+    console.log(currentword);
+    let typegamer = document.getElementById("inputypeword").value.toUpperCase();
+    console.log(" type Gamer " + typegamer);
+    if (currentword == typegamer ) {
+        console.log(" Palabra Igual ");
+        score++;
+        document.getElementById("pointscore").innerHTML=score;
+        randomizewords();
+        document.getElementById("inputypeword").value="";
+    } else{
+        console.log(" Sigue Intentando ");
     }
 }
 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* constante palabras */ /*
-
-const keys = [
-        "dinosaur",
-        "love",
-        "pineapple",
-        "calendar",
-        "robot",
-        "building",
-        "population",
-        "weather",
-        "bottle",
-        "history",
-        "dream",
-        "character",
-        "money",
-        "absolute",
-        "discipline",
-        "machine",
-        "accurate",
-        "connection",
-        "rainbow",
-        "bicycle",
-        "eclipse",
-        "calculator",
-        "trouble",
-        "watermelon",
-        "developer",
-        "philosophy",
-        "database",
-        "periodic",
-        "capitalism",
-        "abominable",
-        "component",
-        "future",
-        "pasta",
-        "microwave",
-        "jungle",
-        "wallet",
-        "canada",
-        "coffee",
-        "beauty",
-        "agency",
-        "chocolate",
-        "eleven",
-        "technology",
-        "alphabet",
-        "knowledge",
-        "magician",
-        "professor",
-        "triangle",
-        "earthquake",
-        "baseball",
-        "beyond",
-        "evolution",
-        "banana",
-        "perfumer",
-        "computer",
-        "management",
-        "discovery",
-        "ambition",
-        "music",
-        "eagle",
-        "crown",
-        "chess",
-        "laptop",
-        "bedroom",
-        "delivery",
-        "enemy",
-        "button",
-        "superman",
-        "library",
-        "unboxing",
-        "bookstore",
-        "language",
-        "homework",
-        "fantastic",
-        "economy",
-        "interview",
-        "awesome",
-        "challenge",
-        "science",
-        "mystery",
-        "famous",
-        "league",
-        "memory",
-        "leather",
-        "planet",
-        "software",
-        "update",
-        "yellow",
-        "keyboard",
-        "window",
-      ];
-
-    document.getElementById("inputypeword").addEventListener("keyup",comparewords);
-    
-    let score = 0;
-
-    function comparewords(){
-        let currentword = document.getElementById("showWords").textContent.toUpperCase();
-        console.log(currentword);
-        let typegamer = document.getElementById("inputypeword").value.toUpperCase();
-        console.log(" type Gamer " + typegamer);
-        if (currentword == typegamer ) {
-            console.log(" Palabra Igual ");
-            score++;
-            document.getElementById("pointscore").innerHTML=score;
-            randomizewords();
-            document.getElementById("inputypeword").value="";
-        } else{
-            console.log(" Sigue Intentando ");
-        }
-    }
-
-
-      function randomizewords() {
-        let originalLen = keys.length;
-
-        const obj = {};
-
-        for (let i = 0; i < originalLen; i++) {
-          let k = Math.floor(Math.random() * keys.length);
-          //console.log(k + " Palabras Aletorias: " + keys[k]);
-          document.getElementById("showWords").innerHTML = keys[k];
-        }
-        console.log(obj);
-      }
-
-      randomizewords();
-
-
-*/
