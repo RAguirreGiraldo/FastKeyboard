571c8764de774a24a8561437914d430e5a5dfc1e
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-30 20:45:34 -0600

    Create function

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,43 +1,103 @@
 'use strict';
-
 /* 
-Funcion 
+document.getElementById("buttonstart").addEventListener("click", startgame)
 
 
+function sumarscore() {
+
+}
+
+let segundos = 60;
+function decrementandorcontador() {
+    document.getElementById("Timeremainder").innerHTML = segundos + "Segundos";
+    if (segundos == 0) {
+        alert('Se termino el tiempo');
+    } else {
+        segundos--;
+        setTimeout('ActualizarTiempo()', 1000);
+    }
+}
+
+// codigo de funcion decrementar tiempo
+let seconds = 60;
+let timer = setInterval(function () {
+    console.log("1 Segundo");
+    seconds--;
+    if (seconds == 0) {
+        stoptimer();
+    }
+    console.log("faltan " + seconds);
+}, 1000);
+
+function stoptimer() {
+    clearInterval(timer);
+}
+
+
+
+
+
+function startgame() {
+    alert('hola');
+    if ("buttonstart" = true) {
+
+    }
+}
 
 
 */
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* constante palabras */ /*
+
 const keys = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
-'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
-'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
-'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
-'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
-'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
-'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
-'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
-'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
-'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
-'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
-'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
-'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
-'keyboard', 'window'];
+    'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+    'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+    'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+    'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+    'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+    'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+    'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+    'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+    'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+    'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+    'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+    'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+    'keyboard', 'window'];
 
-let originalLen = keys.length
+// funcion randomica */
+/*
 
-const obj = {};
+let originalLen = keys.length;
 
-for(let i = 0; i < originalLen; i++){
-  let k = Math.floor(Math.random() * keys.length); 
-  
-  // let v = Math.floor(Math.random() * values.length);
-  
-  obj[keys[k]] = values[v];
-  keys.splice(k,1);
-  values.splice(k,1);
-  
-}
+      const obj = {};
 
-console.clear();
-console.log(obj);
+      for (let i = 0; i < originalLen; i++) {
+        let k = Math.floor(Math.random() * keys.length);
+        console.log(k + " Palabras Aletorias: " + keys[k]);
+        document.getElementById("showWords").innerHTML=keys[k];
+      }
+      console.log(obj);
+
+
+
+
+
+
+*/
